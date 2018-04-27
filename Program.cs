3e0df4207f1e93aefb81342a1517b2d5e732f82d
Pirsohny <31920781+Pirsohny@users.noreply.github.com>
2018-04-27 16:20:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var len = 30;
+
+
+            Turtle.Move(len);
+            Turtle.Turn(90);
+            Turtle.Move(len);
+            Turtle.Turn(90);
+            Turtle.Move(len);
+            Turtle.Turn(90);
+            Turtle.Move(len);
+            Turtle.PenUp();
+            Turtle.Move(len);
+            Turtle.Turn(90);
+            Turtle.Move(len * 2);
+            Turtle.PenDown();
+            Turtle.Turn(90);
+            Turtle.Move(len * 3);
+            Turtle.Turn(90);
+            Turtle.Move(len * 3);
+            Turtle.Turn(90);
+            Turtle.Move(len * 3);
+            Turtle.Turn(90);
+            Turtle.Move(len * 3);
+            Turtle.Turn(30);
+            Turtle.Move(len * 3);
+            Turtle.Turn(120);
+            Turtle.Move(len * 3);
+
+        }
+    }
+}
