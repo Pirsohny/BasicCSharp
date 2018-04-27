56bb3c8d2fef421edab3b5977264481aa8d08f4a
Pirsohny <31920781+Pirsohny@users.noreply.github.com>
2018-04-27 16:25:01 +0300

    HelloName

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "\nРад тебя видеть, " + name);
+            Console.ReadKey();
+
+        }
+    }
+}
